1d773e49111142b618d15f9d434288686605e3cf
kevinamos <kevinongango@gmail.com>
2019-07-16 16:49:46 +0300

    Complete CRUD for exercises

--- mern_exercise_tracker/backend/routes/exercises.js
+++ mern_exercise_tracker/backend/routes/exercises.js
@@ -7,13 +7,14 @@ router.route("/").get((req, res) => {
     .catch(err => res.status(400).json("Error " + err));
 });
 
+//Create
 router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
 
-  const newExercise = new exercise({
+  const newExercise = new Exrecise({
     username,
     description,
     duration,
@@ -23,6 +24,40 @@ router.route("/add").post((req, res) => {
   newExercise
     .save()
     .then(() => res.json("Exercise added"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+//get user by id
+router.route("/:id").get((req, res) => {
+  Exrecise.findById(req.params.id)
+    .then(exr => res.json(exr))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+//Update
+router.route("/update/:id").post((req, res) => {
+  Exrecise.findByIdAndUpdate(req.params.id)
+    .then(exr => {
+      exr.username = req.body.username;
+      exr.description = req.body.description;
+      exr.duration = Number(req.body.duration);
+      exr.date = Date.parse(req.body.date);
+      exr
+        .save()
+        .then(() =>
+          res.json(`exerrcise with id ${req.params.id} has been updated`)
+        )
+        .catch(err => res.status(400).json("Error : " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+//Delete
+router.route("/delete/:id").delete((req, res) => {
+  Exrecise.findByIdAndDelete(req.params.id)
+    .then(exr =>
+      res.json(`exerrcise with id ${req.params.id} has been deleted`)
+    )
     .catch(err => res.status(400).json("Error: " + err));
 });
 
